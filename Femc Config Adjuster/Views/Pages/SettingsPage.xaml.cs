5ea37cb492168e682e8e4dacedeb58e522acbfec
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2024-07-12 22:50:39 -0400

    Add project files.

---
+++ Femc Config Adjuster/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Femc_Config_Adjuster.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace Femc_Config_Adjuster.Views.Pages
+{
+	public partial class SettingsPage : INavigableView<SettingsViewModel>
+	{
+		public SettingsViewModel ViewModel { get; }
+
+		public SettingsPage(SettingsViewModel viewModel)
+		{
+			ViewModel = viewModel;
+			DataContext = this;
+
+			InitializeComponent();
+		}
+	}
+}
