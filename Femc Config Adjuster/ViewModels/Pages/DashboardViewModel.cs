b90e0428fe5e0676f99d7694f4eda1d7dcac735c
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2024-07-16 11:34:54 -0400

    bustup on init

--- Femc Config Adjuster/ViewModels/Pages/DashboardViewModel.cs
+++ Femc Config Adjuster/ViewModels/Pages/DashboardViewModel.cs
@@ -21,23 +21,23 @@ using MessageBox = System.Windows.MessageBox;
 
 namespace Femc_Config_Adjuster.ViewModels.Pages
 {
-    public partial class DashboardViewModel : ObservableObject
+	public partial class DashboardViewModel : ObservableObject
 	{
-        private DashboardPage _mainWindow;
-        private Config _config;
+		private DashboardPage _mainWindow;
+		private Config _config;
 		private string _selectedOptionChoose;
 		private string _selectedOptionInit;
 		private string _selectedOptionNameInit;
-        private ICommand _buttonPressCommand;
+		private ICommand _buttonPressCommand;
 
-        // Define Options array
-        public string[] OptionsInit { get; } = { "Bustup", "AOA", "AOAText", "LevelUp", "Shard", "Cutin"};
-		public ObservableCollection<string> optionchoose { get; } = new ObservableCollection<string> {"Please select a Category to Continue"};
+		// Define Options array
+		public string[] OptionsInit { get; } = { "Bustup", "AOA", "AOAText", "LevelUp", "Shard", "Cutin" };
+		public ObservableCollection<string> optionchoose { get; } = new ObservableCollection<string> { "Please select a Category to Continue" };
 
-        Dictionary<string, Tuple<string, List<string>>> OptionComboSuffix =
-			 new Dictionary<string,Tuple<string,List<string>>>(){
-			 //Entry format {"Option Name", new Tuple<string, List<string>>("Suffix in json file, [Enter all possible options in this list])}
-			 {"Bustup",new Tuple<string, List<string>>("True",["Neptune","Ely","Esa","Betina","Anniversary","JustBlue","Sav","Doodled","RonaldReagan","ElyAlt"])},
+		Dictionary<string, Tuple<string, List<string>>> OptionComboSuffix =
+			 new Dictionary<string, Tuple<string, List<string>>>(){
+             //Entry format {"Option Name", new Tuple<string, List<string>>("Suffix in json file, [Enter all possible options in this list])}
+             {"Bustup",new Tuple<string, List<string>>("True",["Neptune","Ely","Esa","Betina","Anniversary","JustBlue","Sav","Doodled","RonaldReagan","ElyAlt"])},
 			 {"AOA", new Tuple<string,List<string>>("True",["Ely","Chrysanthie","Fernando","Monica","RonaldReagan"])},
 			 {"AOAText", new Tuple<string, List<string>>("",["DontLookBack","SorryBoutThat"])},
 			 {"LevelUp", new Tuple<string, List<string>>("True",["Esa","Ely"])},
@@ -54,26 +54,26 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 			{"ShardTrue",""},
 			{"CutinTrue",""}
 		};
-			                   
-        // Define SelectedOption property
-        public string SelectedOptionInit
+
+		// Define SelectedOption property
+		public string SelectedOptionInit
 		{
 			get => _selectedOptionInit;
 			set
 			{
 				_selectedOptionInit = value;
-                OnPropertyChanged(nameof(SelectedOptionInit));
-                UpdateSelectionCombo();
-            }
+				OnPropertyChanged(nameof(SelectedOptionInit));
+				UpdateSelectionCombo();
+			}
 		}
 
-        public void OnSaveButtonPress()
-        {
-            // Handle button press
-            WriteValRam();
-        }
+		public void OnSaveButtonPress()
+		{
+			// Handle button press
+			WriteValRam();
+		}
 
-        public string SelectedOptionChoose
+		public string SelectedOptionChoose
 		{
 			get => _selectedOptionChoose;
 			set
@@ -84,29 +84,30 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 				{
 					if (CheckOptionExistance(SelectedOptionChoose))
 					{
-							valram[(SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1).ToString()] = SelectedOptionChoose;
-                            SelectedOptionNameInit = valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1];
-                    }
+						valram[(SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1).ToString()] = SelectedOptionChoose;
+						SelectedOptionNameInit = valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1];
+					}
 				}
 			}
 		}
-		
-			private bool CheckOptionExistance(string opt) { 
-			foreach(KeyValuePair<string, Tuple<string, List<string>>> item in OptionComboSuffix)
+
+		private bool CheckOptionExistance(string opt)
+		{
+			foreach (KeyValuePair<string, Tuple<string, List<string>>> item in OptionComboSuffix)
 			{
-                foreach (var iteminlist in OptionComboSuffix[item.Key].Item2)
-                {
+				foreach (var iteminlist in OptionComboSuffix[item.Key].Item2)
+				{
 					if (iteminlist == opt)
 					{
 						return true;
 					}
-                }
-            }
+				}
+			}
 			return false;
-            
-        }
-        // Define SelectedOptionName property
-        public string SelectedOptionNameInit
+
+		}
+		// Define SelectedOptionName property
+		public string SelectedOptionNameInit
 		{
 			get => _selectedOptionNameInit;
 			set
@@ -118,11 +119,13 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 
 		public DashboardViewModel(DashboardPage mainWindow)
 		{
-            LoadConfig();
+			LoadConfig();
 			LoadJsonFile();
-            _mainWindow = mainWindow;
-            GenValRam();
-        }
+			_mainWindow = mainWindow;
+			GenValRam();
+			// Set the default selected option to "Bustup"
+			SelectedOptionInit = "Bustup";
+		}
 
 		private void LoadConfig()
 		{
@@ -154,60 +157,60 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 
 		private void GenValRam()
 		{
-			foreach(KeyValuePair<string,string> item in valram)
+			foreach (KeyValuePair<string, string> item in valram)
 			{
-                var jsonDocument = JsonDocument.Parse(JsonContent);
-                var root = jsonDocument.RootElement;
-                string SelectedOptionInConf = root.GetProperty(item.Key).GetString();
+				var jsonDocument = JsonDocument.Parse(JsonContent);
+				var root = jsonDocument.RootElement;
+				string SelectedOptionInConf = root.GetProperty(item.Key).GetString();
 				if (CheckOptionExistance(SelectedOptionInConf))
 				{
-                    valram[item.Key] = SelectedOptionInConf;
-                }
+					valram[item.Key] = SelectedOptionInConf;
+				}
 				else
 				{
 					string suffrem = item.Key.Replace("True", "");
 					valram[item.Key] = OptionComboSuffix[suffrem].Item2[1];
 				}
-            }
+			}
 		}
 
-        private void WriteValRam()
-        {
-            string jsonString = File.ReadAllText(_config.JsonFilePath);
-            JsonNode jsonObject = JsonNode.Parse(jsonString);
-            foreach (KeyValuePair<string, string> item in valram)
-            {
-                jsonObject[item.Key.ToString()] = item.Value.ToString();
-            }
-            string modifiedJsonString = jsonObject.ToJsonString(new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(_config.JsonFilePath, modifiedJsonString);
-            MessageBox.Show("Game added and JSON data saved!");
-        }
+		private void WriteValRam()
+		{
+			string jsonString = File.ReadAllText(_config.JsonFilePath);
+			JsonNode jsonObject = JsonNode.Parse(jsonString);
+			foreach (KeyValuePair<string, string> item in valram)
+			{
+				jsonObject[item.Key.ToString()] = item.Value.ToString();
+			}
+			string modifiedJsonString = jsonObject.ToJsonString(new JsonSerializerOptions { WriteIndented = true });
+			File.WriteAllText(_config.JsonFilePath, modifiedJsonString);
+			MessageBox.Show("Game added and JSON data saved!");
+		}
 
-        private void UpdateSelectionCombo()
+		private void UpdateSelectionCombo()
 		{
 			try
 			{
-                optionchoose.Clear();
+				optionchoose.Clear();
 				foreach (var item in OptionComboSuffix[SelectedOptionInit].Item2)
 				{
 					optionchoose.Add(item);
 				}
-				if(SelectedOptionInit is null)
+				if (SelectedOptionInit is null)
 				{
 					optionchoose.Add("Generation of Options failed. Please contact Mudkip for help");
-                    _mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf("Generation of Options failed. Please contact Mudkip for help");
-                }
+					_mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf("Generation of Options failed. Please contact Mudkip for help");
+				}
 				else
 				{
-                    _mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf(valram[(SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1).ToString()]);
-                }
+					_mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf(valram[(SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1).ToString()]);
+				}
 				SelectedOptionNameInit = valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1];
-            }
+			}
 			catch (Exception ex)
 			{
-				//Left Blank Temporarily while i figure out a more refined way of showing error messages
+				// Left Blank Temporarily while I figure out a more refined way of showing error messages
 			}
-            }
 		}
 	}
+}
