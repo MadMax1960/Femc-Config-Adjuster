381f1d60bd809d5ed32bcf65b6e90e190b8fb800
TheBestAstroNOT <TheBestAstroNOT@gmail.com>
2024-07-17 08:10:46 +0530

    test commit

--- Femc Config Adjuster/ViewModels/Pages/DashboardViewModel.cs
+++ Femc Config Adjuster/ViewModels/Pages/DashboardViewModel.cs
@@ -30,21 +30,20 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 		private string _selectedOptionInit;
 		private string _selectedOptionNameInit;
 
-
-        // Define Options array
-        public string[] OptionsInit { get; } = { "Bustup", "AOA", "AOAText", "LevelUp", "Shard", "Cutin" };
+		// Define Options array
+		public string[] OptionsInit { get; } = { "Bustup", "AOA", "AOAText", "LevelUp", "Shard", "Cutin" };
 		public ObservableCollection<string> optionchoose { get; } = new ObservableCollection<string> { "Please select a Category to Continue" };
 
 		Dictionary<string, Tuple<string, List<string>>> OptionComboSuffix =
-			 new Dictionary<string, Tuple<string, List<string>>>(){
-             //Entry format {"Option Name", new Tuple<string, List<string>>("Suffix in json file, [Enter all possible options in this list])}
-             {"Bustup",new Tuple<string, List<string>>("True",["Neptune","Ely","Esa","Betina","Anniversary","JustBlue","Sav","Doodled","RonaldReagan","ElyAlt"])},
-			 {"AOA", new Tuple<string,List<string>>("True",["Ely","Chrysanthie","Fernando","Monica","RonaldReagan"])},
-			 {"AOAText", new Tuple<string, List<string>>("",["DontLookBack","SorryBoutThat"])},
-			 {"LevelUp", new Tuple<string, List<string>>("True",["Esa","Ely"])},
-			 {"Shard", new Tuple<string, List<string>>("True",["Esa","Ely"])},
-			 {"Cutin", new Tuple<string, List<string>>("True",["berrycha","ElyandPatmandx"])}
-		};
+			 new Dictionary<string, Tuple<string, List<string>>>()
+			 {
+				 {"Bustup",new Tuple<string, List<string>>("True",new List<string>{"Neptune","Ely","Esa","Betina","Anniversary","JustBlue","Sav","Doodled","RonaldReagan","ElyAlt", "Yuunagi"})},
+				 {"AOA", new Tuple<string,List<string>>("True",new List<string>{"Ely","Chrysanthie","Fernando","Monica","RonaldReagan"})},
+				 {"AOAText", new Tuple<string, List<string>>("",new List<string>{"DontLookBack","SorryBoutThat"})},
+				 {"LevelUp", new Tuple<string, List<string>>("True",new List<string>{"Esa","Ely"})},
+				 {"Shard", new Tuple<string, List<string>>("True",new List<string>{"Esa","Ely"})},
+				 {"Cutin", new Tuple<string, List<string>>("True",new List<string>{"berrycha","ElyandPatmandx"})}
+			 };
 
 		Dictionary<string, string> valram = new Dictionary<string, string>()
 		{
@@ -56,32 +55,33 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 			{"CutinTrue",""}
 		};
 
-		Dictionary<string, Tuple<string,string>> multimediadict = new Dictionary<string, Tuple<string,string>>()
+		Dictionary<string, Tuple<string, string>> multimediadict = new Dictionary<string, Tuple<string, string>>()
 		{
-			{"BustupEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupNeptune",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupEsa",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupBetina",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupAnniversary",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupJustBlue",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupSav",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupDoodled",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupRonaldReagan",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"BustupElyAlt",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOAEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOAChrysanthie",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOAFernando",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOAMonica",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOARonaldReagan",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOATextDontLookBack",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"AOATextSorryBoutThat",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"LevelUpEsa",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"LevelUpEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"ShardEsa",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"ShardEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"Cutinberrycha",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-            {"CutinElyandPatmandx",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
-        };
+			{"BustupEly",new Tuple<string, string>("This was made by @Ely", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/Screenshot_305.png?raw=true")},
+			{"BustupNeptune",new Tuple<string, string>("This was made by @Neptune_NPN013", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/Screenshot_304.png?raw=true")},
+			{"BustupEsa",new Tuple<string, string>("This was made by @EsaBlythe", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/Screenshot_306.png?raw=true")},
+			{"BustupBetina",new Tuple<string, string>("This was made by @Betina_Mascenon", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/betina.png?raw=true")},
+			{"BustupAnniversary",new Tuple<string, string>("Official P25 Art edited by @EsaBlythe", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/anniversary.png?raw=true")},
+			{"BustupJustBlue",new Tuple<string, string>("This was made by justblueowo", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/just%20blue.png?raw=true")},
+			{"BustupSav",new Tuple<string, string>("This was made by @fugoluv3r", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/sav.png?raw=true")},
+			{"BustupDoodled",new Tuple<string, string>("This was made by @NotDoodled", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/doodled.png?raw=true")},
+			{"BustupRonaldReagan",new Tuple<string, string>("This was made by @CatboyRonReagan", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/ronaldreagen.png?raw=true")},
+			{"BustupElyAlt",new Tuple<string, string>("This was made by Ely", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/elyalt.png?raw=true")},
+			{"BustupYuunagi",new Tuple<string, string>("This was made by Yuunagi", "https://github.com/MadMax1960/Femc-Config-Adjuster/blob/master/ui/bustup/Yuunagi.png?raw=true")},
+			{"AOAEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"AOAChrysanthie",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"AOAFernando",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"AOAMonica",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"AOARonaldReagan",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"AOATextDontLookBack",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"AOATextSorryBoutThat",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"LevelUpEsa",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"LevelUpEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"ShardEsa",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"ShardEly",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"Cutinberrycha",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+			{"CutinElyandPatmandx",new Tuple<string, string>("Dummy Description", "https://raw.githubusercontent.com/MadMax1960/Femc-Reloaded-Project/main/img/readmelogo.png")},
+		};
 
 		// Define SelectedOption property
 		public string SelectedOptionInit
@@ -108,33 +108,10 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 			{
 				_selectedOptionChoose = value;
 				OnPropertyChanged(nameof(SelectedOptionChoose));
-				if (SelectedOptionChoose != null)
-				{
-					if (CheckOptionExistance(SelectedOptionChoose))
-					{
-						valram[(SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1).ToString()] = SelectedOptionChoose;
-						SelectedOptionNameInit = valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1];
-					}
-				}
+				UpdateSelectionDetails();
 			}
 		}
-        
 
-        private bool CheckOptionExistance(string opt)
-		{
-			foreach (KeyValuePair<string, Tuple<string, List<string>>> item in OptionComboSuffix)
-			{
-				foreach (var iteminlist in OptionComboSuffix[item.Key].Item2)
-				{
-					if (iteminlist == opt)
-					{
-						return true;
-					}
-				}
-			}
-			return false;
-
-		}
 		// Define SelectedOptionName property
 		public string SelectedOptionNameInit
 		{
@@ -196,7 +173,7 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 				else
 				{
 					string suffrem = item.Key.Replace("True", "");
-					valram[item.Key] = OptionComboSuffix[suffrem].Item2[1];
+					valram[item.Key] = OptionComboSuffix[suffrem].Item2[0];
 				}
 			}
 		}
@@ -223,23 +200,50 @@ namespace Femc_Config_Adjuster.ViewModels.Pages
 				{
 					optionchoose.Add(item);
 				}
-				if (SelectedOptionInit is null)
-				{
-					optionchoose.Add("Generation of Options failed. Please contact Mudkip for help");
-					_mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf("Generation of Options failed. Please contact Mudkip for help");
-				}
-				else
-				{
-					_mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf(valram[(SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1).ToString()]);
-				}
-				SelectedOptionNameInit = valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1];
-				SelectedOptionNameInit = multimediadict[SelectedOptionInit + SelectedOptionChoose].Item1;
-                _mainWindow.descimage.Source = new BitmapImage(new Uri(multimediadict[SelectedOptionInit + SelectedOptionChoose].Item2));
-            }
+				_mainWindow.ChangeValue.SelectedIndex = _mainWindow.ChangeValue.Items.IndexOf(valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1]);
+				UpdateSelectionDetails();
+			}
 			catch (Exception ex)
 			{
-				// Left Blank Temporarily while I figure out a more refined way of showing error messages
+				// Handle exception
+				Debug.WriteLine($"UpdateSelectionCombo Error: {ex.Message}");
 			}
+		}
+
+		private void UpdateSelectionDetails()
+		{
+			try
+			{
+				if (SelectedOptionChoose != null)
+				{
+					if (CheckOptionExistance(SelectedOptionChoose))
+					{
+						valram[SelectedOptionInit + OptionComboSuffix[SelectedOptionInit].Item1] = SelectedOptionChoose;
+						SelectedOptionNameInit = multimediadict[SelectedOptionInit + SelectedOptionChoose].Item1;
+						_mainWindow.descimage.Source = new BitmapImage(new Uri(multimediadict[SelectedOptionInit + SelectedOptionChoose].Item2));
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				// Handle exception
+				Debug.WriteLine($"UpdateSelectionDetails Error: {ex.Message}");
+			}
+		}
+
+		private bool CheckOptionExistance(string opt)
+		{
+			foreach (KeyValuePair<string, Tuple<string, List<string>>> item in OptionComboSuffix)
+			{
+				foreach (var iteminlist in OptionComboSuffix[item.Key].Item2)
+				{
+					if (iteminlist == opt)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
 		}
 	}
 }
