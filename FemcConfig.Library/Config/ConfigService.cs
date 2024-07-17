e57484cfffb9706d4320daf844404cae03e8a599
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-17 06:39:16 -0500

    Add config service to automatically find and load FEMC mod config.

---
+++ FemcConfig.Library/Config/ConfigService.cs
@@ -0,0 +1,70 @@
+﻿using FemcConfig.Library.Common;
+using FemcConfig.Library.Config.Models;
+using FemcConfig.Library.Utils;
+
+namespace FemcConfig.Library.Config;
+
+public class ConfigService
+{
+    private ModContext? modContext;
+
+    public ConfigService()
+    {
+        this.AutoInit();
+    }
+
+    private void AutoInit()
+    {
+        // Get Reloaded dir from environment.
+        var reloadedModsDir = Environment.GetEnvironmentVariable("RELOADEDIIMODS")
+            ?? throw new Exception("Failed to find Reloaded II ENV variable.");
+
+        var reloadedDir = Path.GetDirectoryName(reloadedModsDir)!;
+
+        // Verify FEMC mod install dir.
+        // Manually search for FEMC DLL since folder name isn't constant.
+        string? femcDir = null;
+        foreach (var dir in Directory.EnumerateDirectories(reloadedModsDir))
+        {
+            var femcDll = Path.Join(dir, "p3rpc.femc.dll");
+            if (File.Exists(femcDll))
+            {
+                femcDir = dir;
+                break;
+            }
+        }
+
+        if (femcDir == null)
+        {
+            throw new Exception("Failed to find FEMC dir.");
+        }
+
+        // Find FEMC mod config file.
+        var reloadedConfigsDir = Path.Join(reloadedDir, "user", "mods");
+        string? femcConfigFile = null;
+        foreach (var configDir in Directory.EnumerateDirectories(reloadedConfigsDir))
+        {
+            var userConfigFile = Path.Join(configDir, "ModUserConfig.json"); 
+            var userConfig = JsonUtils.DeserializeFile<ReloadedModUserConfig>(userConfigFile);
+
+            if (userConfig.ModId == Constants.FEMC_MOD_ID)
+            {
+                femcConfigFile = Path.Join(configDir, "Config.json");
+                break;
+            }
+        }
+
+        if (femcConfigFile == null || File.Exists(femcConfigFile) == false)
+        {
+            throw new Exception("Failed to find FEMC config file.");
+        }
+
+        // Setup mod context.
+        this.modContext = new()
+        {
+            ReloadedDir = reloadedDir,
+            ModDir = femcDir,
+            ModConfig = new(femcConfigFile),
+        };
+    }
+}
---
+++ FemcConfig.Library/Config/ModContext.cs
@@ -0,0 +1,12 @@
+﻿using FemcConfig.Library.Config.Models;
+
+namespace FemcConfig.Library.Config;
+
+public class ModContext
+{
+    public required string ReloadedDir { get; init; }
+
+    public required string ModDir { get; init; }
+
+    public required ModConfig ModConfig { get; init; }
+}
