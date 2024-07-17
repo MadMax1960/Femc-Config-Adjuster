4f24a3a21fff712c8929e07208458666352f0be3
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-17 06:39:37 -0500

    Add library and JsonUtils.

---
+++ FemcConfig.Library/Utils/JsonUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace FemcConfig.Library.Utils;
+
+public static class JsonUtils
+{
+    public static T DeserializeFile<T>(string file)
+    {
+        var obj = JsonSerializer.Deserialize<T>(File.ReadAllText(file));
+        return obj ?? throw new Exception($"Failed to deserialize file.\nFile: {file}");
+    }
+
+    public static void SerializeFile<T>(T obj, string file)
+    {
+        var objText = JsonSerializer.Serialize(obj);
+        File.WriteAllText(file, objText);
+    }
+}
