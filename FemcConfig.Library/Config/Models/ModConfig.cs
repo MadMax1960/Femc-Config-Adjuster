d9259bda09daf5b2de3c7d25e3b7f64ca2ea5fbf
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-17 22:26:16 -0500

    Add autosave on changes to config. Add example of wrapping non-observable enum property as observable.

--- FemcConfig.Library/Config/Models/ModConfig.cs
+++ FemcConfig.Library/Config/Models/ModConfig.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using FemcConfig.Library.Utils;
+using static FemcConfig.Library.Config.Models.ReloadedModConfig;
 
 namespace FemcConfig.Library.Config.Models;
 
@@ -12,7 +13,27 @@ public class ModConfig : ObservableObject
     {
         this.configFile = configFile;
         this.modConfig = JsonUtils.DeserializeFile<ReloadedModConfig>(configFile);
+        this.PropertyChanged += (sender, args) =>
+        {
+            try
+            {
+                // TODO: Create some sort of backup incase of error.
+                this.Save();
+            }
+            catch (Exception)
+            {
+                // TODO: Display error message.
+            }
+        };
     }
+
+    public nightmusic1 NightMusic
+    {
+        get => this.modConfig.nighttrue1;
+        set => this.SetProperty(this.modConfig.nighttrue1, value, this.modConfig, (c, n) => c.nighttrue1 = n);
+    }
+
+    public ReloadedModConfig Settings => this.modConfig;
 
     public void Save()
     {
