17aa7d571c893bf4578eedf74b184aee8a1dc22a
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-17 22:38:18 -0500

    Add converter for author list to simple string.

---
+++ Femc Config Adjuster/Helpers/AuthorListToString.cs
@@ -0,0 +1,21 @@
+﻿using FemcConfig.Library.Config.Options;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Femc_Config_Adjuster.Helpers;
+
+public class AuthorListToString : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IEnumerable<Author> authors)
+        {
+            return string.Join(", ", authors.Select(x => x.Name));
+        }
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => value;
+}
