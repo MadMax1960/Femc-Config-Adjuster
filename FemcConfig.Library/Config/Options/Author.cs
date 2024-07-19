31264415fc8531cef5d2e1997060e5049b3d9902
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-19 06:53:13 -0500

    Add currently used authors.

--- FemcConfig.Library/Config/Options/Author.cs
+++ FemcConfig.Library/Config/Options/Author.cs
@@ -1,3 +1,17 @@
 ﻿namespace FemcConfig.Library.Config.Options;
 
-public record Author(string Name, string? Url = null);
+public record Author(string Name, string? Url = null)
+{
+    public static readonly Author Missing = new("Missing Author");
+
+    public static readonly Author Neptune = new("Neptune");
+    public static readonly Author Ely = new("Ely");
+    public static readonly Author Esa = new("Esa");
+    public static readonly Author Betina = new("Betina");
+    public static readonly Author Anniversary = new("Anniversary");
+    public static readonly Author JustBlue = new("Just Blue");
+    public static readonly Author Sav = new("Sav");
+    public static readonly Author Doodled = new("Doodled");
+    public static readonly Author Ronald = new("Ronald Reagan");
+    public static readonly Author Yuunagi = new("Yuunagi");
+};
