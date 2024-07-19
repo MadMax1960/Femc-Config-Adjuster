5c7e42534f0d48e348267dac44c7aaf5ee1f6035
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-19 06:15:29 -0500

    Add converter for getting mod option image.

---
+++ Femc Config Adjuster/Helpers/OptionImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using FemcConfig.Library.Config.Options;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace Femc_Config_Adjuster.Helpers;
+
+public class OptionImageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var appDir = AppDomain.CurrentDomain.BaseDirectory;
+        if (value is ModOption option)
+        {
+            var imagePath = Path.Join(appDir, "resources", option.InternalName, "image.webp");
+            if (File.Exists(imagePath))
+            {
+                return imagePath;
+            }
+
+            return Path.Join(AppDomain.CurrentDomain.BaseDirectory, "resources", "missing.png");
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
