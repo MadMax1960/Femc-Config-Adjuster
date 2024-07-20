992b9661f9d4306907ad22e19e19ae003d722152
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-20 06:09:38 -0500

    Add category VM.

---
+++ Femc Config Adjuster/ViewModels/Pages/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FemcConfig.Library.Config.Sections;
+
+namespace Femc_Config_Adjuster.ViewModels.Pages;
+
+public class CategoryViewModel
+{
+    public CategoryViewModel(ISection[] sections)
+    {
+        this.Sections = sections;
+    }
+
+    public ISection[] Sections { get; }
+}
