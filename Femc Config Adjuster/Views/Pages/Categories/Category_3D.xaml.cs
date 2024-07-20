04984b935e3c8cbfe405353977a9629363636cce
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-20 06:08:32 -0500

    Add pages for each setting category.

---
+++ Femc Config Adjuster/Views/Pages/Categories/Category_3D.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Femc_Config_Adjuster.ViewModels.Pages;
+using FemcConfig.Library.Config.Sections;
+using System.Windows.Controls;
+
+namespace Femc_Config_Adjuster.Views.Pages.Categories;
+
+/// <summary>
+/// Interaction logic for Category_3D.xaml
+/// </summary>
+public partial class Category_3D : Page
+{
+    public Category_3D(ISection[] sections)
+    {
+        InitializeComponent();
+        this.DataContext = new CategoryViewModel(sections.Where(x => x.Category == SectionCategory.ThreeD).ToArray());
+    }
+}
---
+++ Femc Config Adjuster/Views/Pages/Categories/Category_Audio.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Femc_Config_Adjuster.ViewModels.Pages;
+using FemcConfig.Library.Config.Sections;
+using System.Windows.Controls;
+
+namespace Femc_Config_Adjuster.Views.Pages.Categories;
+
+/// <summary>
+/// Interaction logic for Category_Audio.xaml
+/// </summary>
+public partial class Category_Audio : Page
+{
+    public Category_Audio(ISection[] sections)
+    {
+        InitializeComponent();
+        this.DataContext = new CategoryViewModel(sections.Where(x => x.Category == SectionCategory.Audio).ToArray());
+    }
+}
