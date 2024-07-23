030ccae94e595ea68ba9ff3459488dbaba88c5af
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-23 09:14:06 -0500

    Implement image preview.

---
+++ Femc Config Adjuster/ViewModels/Windows/PreviewWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Femc_Config_Adjuster.Helpers;
+using FemcConfig.Library.Config.Options;
+
+namespace Femc_Config_Adjuster.ViewModels.Windows;
+
+internal class PreviewWindowViewModel
+{
+    public PreviewWindowViewModel(ModOption option)
+    {
+        this.Option = option;
+        this.Title = option.Name ?? string.Join(", ", option.Authors.Select(x => x.Name));
+        this.ImagePath = ResourceUtils.GetOptionImagePath(option, true);
+    }
+
+    public ModOption Option { get; }
+
+    public string Title { get; }
+
+    public string ImagePath { get; }
+}
---
+++ Femc Config Adjuster/Views/Windows/PreviewWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Femc_Config_Adjuster.ViewModels.Windows;
+using System.Windows;
+using Wpf.Ui.Controls;
+
+namespace Femc_Config_Adjuster.Views.Windows;
+
+/// <summary>
+/// Interaction logic for PreviewWindow.xaml
+/// </summary>
+public partial class PreviewWindow : FluentWindow
+{
+    public PreviewWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (this.DataContext is PreviewWindowViewModel vm)
+        {
+            vm.Option.IsEnabled = true;
+        }
+
+        this.Close();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
