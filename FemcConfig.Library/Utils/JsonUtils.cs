690f07bbb7525d11445e7ad81c153d6d6cd6855f
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2024-07-24 16:57:47 +0530

    Fixed the crashes caused by the json being damaged or missing. If an error now occurs related to the json it will be overwritten by a sample json.

--- FemcConfig.Library/Utils/JsonUtils.cs
+++ FemcConfig.Library/Utils/JsonUtils.cs
@@ -15,7 +15,28 @@ public static class JsonUtils
     public static T DeserializeFile<T>(string file)
     {
         var obj = JsonSerializer.Deserialize<T>(File.ReadAllText(file), serializerOptions);
-        return obj ?? throw new Exception($"Failed to deserialize file.\nFile: {file}");
+        // If deserialization succeeds, return the object
+        if (obj != null)
+        {
+            return obj;
+        }
+        else
+        {
+            string backupjsonstr = "string jsonbackup = \"{\\r\\n  \\\"mosq\\\": false,\\r\\n  \\\"mosqeidk\\\": true,\\r\\n  \\\"karma\\\": false,\\r\\n  \\\"rock\\\": false,\\r\\n  \\\"nighttrue1\\\": \\\"TimeNightByMosqGabiVer\\\",\\r\\n  \\\"dayintrue1\\\": \\\"TimeByMosqGabiVer\\\",\\r\\n  \\\"dayintrue2\\\": \\\"SunByMosq\\\",\\r\\n  \\\"dayouttrue1\\\": \\\"WayOfL";
+            // If deserialization fails, replace the file content with the stored string
+            File.WriteAllText(file,backupjsonstr);
+
+            // Deserialize the new file content
+            obj = JsonSerializer.Deserialize<T>(File.ReadAllText(file), serializerOptions);
+
+            // If deserialization of the stored string fails, throw an exception
+            if (obj == null)
+            {
+                throw new Exception($"Failed to deserialize stored string.\nStored String: {backupjsonstr}");
+            }
+
+            return obj;
+        }
     }
 
     public static void SerializeFile<T>(T obj, string file)
