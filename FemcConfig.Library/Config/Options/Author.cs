0d49829869834d7cc1d8dd8c832bdc0d87ede255
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2024-07-29 17:17:49 -0400

    links 2

--- FemcConfig.Library/Config/Options/Author.cs
+++ FemcConfig.Library/Config/Options/Author.cs
@@ -16,10 +16,10 @@ public record Author(string Name, string? Description = null, string? Url = null
     public static readonly Author Yuunagi = new("Yuunagi", Url: "https://www.instagram.com/35yuunagi");
     public static readonly Author Berrycha = new("berrycha", Url: "https://x.com/rmeiz147");
     public static readonly Author PatMandDX = new("PatManDX", Url: "https://x.com/PatManDX");
-    public static readonly Author Chrysanthie = new("Chrysanthie", Url: "https://x.com/PatManDX");
-    public static readonly Author Fernando = new("Fernando", Url: "https://x.com/PatManDX");
-    public static readonly Author Monica = new("Monica", Url: "https://x.com/PatManDX");
-    public static readonly Author Mosq = new("Mosq", Url: "https://x.com/PatManDX");
-    public static readonly Author Atlus = new("Atlus", Url: "https://x.com/PatManDX");
-    public static readonly Author Mudkip = new("MadMax1960", Url: "https://x.com/PatManDX");
+    public static readonly Author Chrysanthie = new("Chrysanthie", Url: "https://x.com/corodaroro");
+    public static readonly Author Fernando = new("Fernando", Url: "https://x.com/HolguinDev");
+    public static readonly Author Monica = new("Monica", Url: "https://x.com/MonikArtZ");
+    public static readonly Author Mosq = new("Mosq", Url: "https://x.com/MOSQmusic");
+    public static readonly Author Atlus = new("Atlus", Url: "Atlus.com");
+    public static readonly Author Mudkip = new("MadMax1960");
 };
