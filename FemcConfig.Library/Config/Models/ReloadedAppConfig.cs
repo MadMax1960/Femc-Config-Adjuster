a8038b935baa23c54a2afee1831514685453a191
RyoTune <zutomayofan50@gmail.com>
2024-08-28 12:14:54 -0500

    Suppress warnings.

--- FemcConfig.Library/Config/Models/ReloadedAppConfig.cs
+++ FemcConfig.Library/Config/Models/ReloadedAppConfig.cs
@@ -1,47 +1,47 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 
-namespace FemcConfig.Library.Config.Models
+namespace FemcConfig.Library.Config.Models;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+public partial class ReloadedAppConfig : ObservableObject
 {
-    public partial class ReloadedAppConfig : ObservableObject
-    {
-        [ObservableProperty]
-        private string appId;
+    [ObservableProperty]
+    private string appId;
 
-        [ObservableProperty]
-        private string appName;
+    [ObservableProperty]
+    private string appName;
 
-        [ObservableProperty]
-        private string appLocation;
+    [ObservableProperty]
+    private string appLocation;
 
-        [ObservableProperty]
-        private string appArguments;
+    [ObservableProperty]
+    private string appArguments;
 
-        [ObservableProperty]
-        private string appIcon;
+    [ObservableProperty]
+    private string appIcon;
 
-        [ObservableProperty]
-        private bool autoInject;
+    [ObservableProperty]
+    private bool autoInject;
 
-        [ObservableProperty]
-        private ObservableCollection<string> enabledMods;
+    [ObservableProperty]
+    private ObservableCollection<string> enabledMods;
 
-        [ObservableProperty]
-        private string workingDirectory;
+    [ObservableProperty]
+    private string workingDirectory;
 
-        [ObservableProperty]
-        private Dictionary<string, Dictionary<string, int>> pluginData;
+    [ObservableProperty]
+    private Dictionary<string, Dictionary<string, int>> pluginData;
 
-        [ObservableProperty]
-        private List<string> sortedMods;
+    [ObservableProperty]
+    private List<string> sortedMods;
 
-        [ObservableProperty]
-        private bool preserveDisabledModOrder;
+    [ObservableProperty]
+    private bool preserveDisabledModOrder;
 
-        [ObservableProperty]
-        private bool dontInject;
+    [ObservableProperty]
+    private bool dontInject;
 
-        [ObservableProperty]
-        private bool isMsStore;
-    }
+    [ObservableProperty]
+    private bool isMsStore;
 }
