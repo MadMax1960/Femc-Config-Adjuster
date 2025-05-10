e443ddb8e25490a3950c8a7b379e1e0763e8b0a9
raycopper <kingdom.hearts@live.dk>
2025-05-10 09:24:17 +0200

    Version bump

--- FemcConfig.Library/Common/Constants.cs
+++ FemcConfig.Library/Common/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string FEMC_MOD_VER = "1.11.0";
+    public const string FEMC_MOD_VER = "1.12.0";
     public const string FEMC_MOD_ID = "p3rpc.femc";
     public const string MOVIE_MOD_ID = "Persona_3_Reload_Intro_Movies";
 }
